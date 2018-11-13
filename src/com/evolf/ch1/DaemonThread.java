b7461f5d90e724e3fdbcc4033326fae5d222d48b
hevolf <caohaifengx@163.com>
2018-11-13 15:12:26 +0800

    inital

--- src/com/evolf/ch1/DaemonThread.java
+++ src/com/evolf/ch1/DaemonThread.java
@@ -3,9 +3,9 @@ package com.evolf.ch1;
 import java.util.concurrent.ExecutionException;
 
 /**
- *@author Mark老师   享学课堂 https://enjoy.ke.qq.com 
+ *@author caohaifeng
  *
- *更多课程咨询 安生老师 QQ：669100976  VIP课程咨询 依娜老师  QQ：2470523467
+ *
  *
  *类说明：守护线程的使用
  */
