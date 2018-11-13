324ff54344463dbc1ef7ba4890ea92c5bf84422d
hevolf <caohaifengx@163.com>
2018-11-14 00:24:34 +0800

    AQS

---
+++ src/com/evolf/ch4_AQS/aqs/SelfLock01.java
@@ -0,0 +1,52 @@
+package com.evolf.ch4_AQS.aqs;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class SelfLock01 implements Lock {
+    private static class Sync extends AbstractQueuedSynchronizer{
+        protected boolean isHeldExclusively() {
+            return getState()==1;
+        }
+
+        protected boolean tryAcquire(int arg) {
+            if(compareAndSetState(0,1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public void unlock() {
+
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+}
