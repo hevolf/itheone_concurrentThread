26d28376fb8ff796d250c7cdd957ec76a43dbe04
hevolf <caohaifengx@163.com>
2018-12-04 17:54:06 +0800

    结构优化

--- src/com/evolf/ch5/blockingqueue/ItemVo.java
+++ src/com/evolf/ch5/blockingqueue/ItemVo.java
@@ -11,14 +11,14 @@ import java.util.concurrent.TimeUnit;
 public class ItemVo<T> implements Delayed{
 	
 	private long activeTime;//到期时间，单位毫秒
-	private T date;
+	private T data;
 	
 	//activeTime是个过期时长
 	public ItemVo(long activeTime, T date) {
 		super();
 		this.activeTime = TimeUnit.NANOSECONDS.convert(activeTime,
 				TimeUnit.MILLISECONDS)+System.nanoTime();//将传入的时长转换为超时的时刻 TimeUnit.NANOSECONDS-转换后单位     convert(待转换值, 待转化值的单位)
-		this.date = date;
+		this.data = date;
 	}
 	
 	public long getActiveTime() {
@@ -26,7 +26,7 @@ public class ItemVo<T> implements Delayed{
 	}
 
 	public T getDate() {
-		return date;
+		return data;
 	}
 
 	//按照剩余时间排序
